513b003e690f43b1d92bbbaeda7ce86714f4a9be
Nguyen Phuc Vinh/Game Development team/SG <phucvinh.nguyen@nhnent.com>
2017-10-04 09:21:08 +0700

    + Create table category

---
+++ database/migrations/2017_09_29_081005_Category.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Category extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
